940266e8c8fa47a6c0128ce047fac0a363a57e35
Manh Nguyen <manhnguyen0164@gmail.com>
2022-06-07 02:18:15 +0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        //TODO: demo serialization
+        User user1 = new User(1, "the_first", 16, new address("Nam Tu Liem", "Ha Noi"), sex.MALE);
+    }
+}
